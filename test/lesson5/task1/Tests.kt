a3a583140f899d4da755214b3a3b01aa36e05e3d
avtuhovich.ns <ipfelps@gmail.com>
2017-12-06 13:39:25 +0200

    error

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -79,8 +79,6 @@ class Tests {
         assertEquals(226, bestHighJump("226 +"))
         assertEquals(-1, bestHighJump("???"))
         assertEquals(230, bestHighJump("220 + 224 %+ 228 %- 230 + 232 %%- 234 %"))
-        assertEquals(789, bestHighJump("%- 789 + 6"))
-        assertEquals(789, bestHighJump("%- 789 + 6+"))
     }
 
     @Test
